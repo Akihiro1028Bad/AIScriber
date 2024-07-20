0b3376d99a82c514d874cd5f192c211af097c8c3
TUTUMI\ttmak <ttmakhr1028@gmail.com>
2024-07-20 19:45:03 +0900

    アプリケーション起動方法修正

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # app.py
-
+import os
 from flask_socketio import SocketIO
 from config import create_app
 from routes import register_routes
@@ -15,4 +15,5 @@ register_routes(app, socketio)
 
 if __name__ == '__main__':
     # アプリケーションの実行
-    socketio.run(app, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
