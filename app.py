54d7270ba3f56cb233a15fb7f3c1023daa324d15
TUTUMI\ttmak <ttmakhr1028@gmail.com>
2024-07-20 22:01:01 +0900

    非同期処理修正

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
 # app.py
+from gevent import monkey
+monkey.patch_all()
+
 import os
 from flask_socketio import SocketIO
 from config import create_app
 from routes import register_routes
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 
 # アプリケーションの作成
 app = create_app()
+
+# Redis URLの設定
+redis_url = os.getenv('REDIS_URL', 'redis://localhost:6379')
+
+# Flask-Limiterの設定
+limiter = Limiter(
+    get_remote_address,
+    app=app,
+    storage_uri=redis_url
+)
 
 # Socket.IOの初期化
 socketio = SocketIO(app)
@@ -14,6 +29,8 @@ socketio = SocketIO(app)
 register_routes(app, socketio)
 
 if __name__ == '__main__':
-    # アプリケーションの実行
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    port = int(os.getenv("PORT", 5000))
+    from gevent import pywsgi
+    from geventwebsocket.handler import WebSocketHandler
+    server = pywsgi.WSGIServer(('0.0.0.0', port), app, handler_class=WebSocketHandler)
+    server.serve_forever()
